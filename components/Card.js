b728da2484848eaa9a1c72bd67abd0f66dbc8278
songtoantd18@gmail.com <songtoan>
2024-04-17 13:33:22 +0700

    ui ux ok

---
+++ components/Card.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { View, Text, Image, StyleSheet, TouchableOpacity } from "react-native";
+
+const Card = ({ itemData, onPress }) => {
+  return (
+    <TouchableOpacity style={styles.categoryBtn} onPress={onPress}>
+      <Text style={styles.categoryBtnTxt}>{itemData.name}</Text>
+    </TouchableOpacity>
+  );
+};
+export default Card;
+
+const styles = StyleSheet.create({
+  categoryBtn: {
+    alignSelf: "center",
+    backgroundColor: "#AED6F1" /* '#FF6347' */,
+    borderRadius: 40,
+    marginBottom: 10,
+    width: "90%",
+    height: 60,
+    marginTop: 10,
+  },
+  categoryBtnTxt: {
+    alignSelf: "center",
+    color: "#1F618D",
+    fontWeight: "bold",
+    paddingHorizontal: 20,
+    textAlign: "center",
+    paddingVertical: 15,
+    fontSize: 18,
+  },
+});
--- screen/HomeScreen.js
+++ screen/HomeScreen.js
@@ -14,27 +14,27 @@ import { useTheme } from "@react-navigation/native";
 
 import Card from "../components/Card";
 
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
 const HomeScreen = ({ navigation }) => {
   const [data, setData] = useState([]);
-  const [responseData, setResponseData] = useState({});
-  const { colors } = useTheme();
+  const fullname = {};
+  let [responseData, setResponseData] = React.useState("");
   const axios = require("axios").default;
-
+  const readinfo = async () => {
+    const jsonValue = await AsyncStorage.getItem("@storage_Key");
+    let jsonObject = { user_login: "toanns", user_password: "qqq111!@#" };
+    return jsonObject;
+  };
   useEffect(() => {
-    const readInfo = async () => {
-      const jsonValue = {
-        user_login: "toanns",
-        user_password: "qqq111!@#",
-      };
-      return jsonValue;
-    };
-
-    readInfo().then((jsonObject) => {
+    readinfo().then((jsonObject) => {
       let url_request =
-        "http://epur.unicjsc.com/menu_eprc/mobile/getmenudefault?username=" +
+        "https://epur.unicjsc.com/menu_eprc/mobile/getmenudefault?username=" +
         jsonObject.user_login +
         "&password=" +
         jsonObject.user_password;
+      console.log("🚀 ~ readinfo ~ url_request:", url_request);
+
       axios.get(url_request).then(({ data }) => {
         // console.log(data);
         setData(data);
@@ -43,8 +43,8 @@ const HomeScreen = ({ navigation }) => {
         method: "POST",
         url: "https://epur.unicjsc.com/user_eprc/login",
         data: {
-          user_login: "toanns",
-          user_password: "qqq111!@#",
+          user_login: jsonObject.user_login,
+          user_password: jsonObject.user_password,
         },
         headers: {
           Accept: "application/json",
@@ -52,8 +52,7 @@ const HomeScreen = ({ navigation }) => {
         },
       })
         .then(function (response) {
-          console.log("🚀 ~ response:", response);
-          console.log(response.data.fullname);
+          console.log(response.data);
           setResponseData(response.data);
         })
         .catch(function (error) {
@@ -63,16 +62,19 @@ const HomeScreen = ({ navigation }) => {
   }, []);
 
   const renderItem = ({ item }) => {
+    console.log("🚀 ~ renderItem ~ item:", item);
     return (
       <Card
         itemData={item}
         onPress={() =>
-          navigation.navigate("CardItemDetils", { itemData: item })
+          navigation.navigate("CardItemDetails", { itemData: item })
         }
       ></Card>
     );
   };
-
+  const theme = useTheme();
+  // console.log("🚀 ~ HomeScreen ~ data:", data);
+  // console.log("🚀 ~ renderItem ~ renderItem:", renderItem(data[0]));
   return (
     <View style={styles.container}>
       <StatusBar hidden />
@@ -80,17 +82,11 @@ const HomeScreen = ({ navigation }) => {
         source={require("../assets/homepageBG.png")}
         style={styles.topimage}
       >
-        <Text style={[styles.topName, { color: colors.text }]}>
-          Hi, {responseData.fullname}!
-        </Text>
-        <Text style={[styles.topPosition, { color: colors.text }]}>
-          {responseData.department_name}
-        </Text>
-        <Text style={[styles.topPosition, { color: colors.text }]}>
-          {responseData.email}
-        </Text>
+        <Text style={styles.topName}>Hi, {responseData.fullname}!</Text>
+        <Text style={styles.topPosition}>{responseData.department_name}</Text>
+        <Text style={styles.topPosition}>{responseData.email}</Text>
       </ImageBackground>
-      <Text style={[styles.Header, { color: colors.text }]}>Categories</Text>
+      <Text style={styles.Header}>Categories</Text>
       <FlatList
         style={styles.itemRenderContainer}
         data={data.data}
@@ -100,14 +96,15 @@ const HomeScreen = ({ navigation }) => {
     </View>
   );
 };
-
 export default HomeScreen;
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
   itemRenderContainer: {
+    display: "flex",
+    width: "100%",
+    alignSelf: "center",
     marginTop: 25,
     marginBottom: 10,
   },
@@ -116,20 +113,34 @@ const styles = StyleSheet.create({
     width: "100%",
   },
   topName: {
+    color: "white",
     fontSize: 25,
     paddingTop: 20,
     paddingLeft: 18,
     fontWeight: "bold",
   },
+  oneLineInfo: {
+    display: "flex",
+    justifyContent: "flex-start",
+    width: "100%",
+  },
   topPosition: {
+    color: "white",
     fontSize: 15,
     paddingLeft: 18,
     fontWeight: "600",
     paddingTop: 5,
   },
   Header: {
+    color: "#434343",
     fontSize: 22,
     paddingLeft: 18,
     fontWeight: "bold",
+  },
+  categoryBtn: {
+    flex: 1,
+    width: "30%",
+    marginHorizontal: 0,
+    alignSelf: "center",
   },
 });
